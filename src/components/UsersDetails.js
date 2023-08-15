30ef8a4b4c33192f136caf4313acb318091168dc
Rony <ronytalukder0014@gmail.com>
2023-08-16 03:48:23 +0600

    add master layout

---
+++ src/components/UsersDetails.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const UsersDetails = () => {
+    
+    return (
+        <div>
+            <h1>This is Users Details</h1>
+        </div>
+    );
+};
+
+export default UsersDetails;
